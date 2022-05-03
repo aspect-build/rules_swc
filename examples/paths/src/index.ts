5d7dcef9b05370d99eb2ff29bda3e0b41563488d
Victor <victorgustafsson@hotmail.com>
2022-05-03 10:53:56 +0200

    feat: add paths repro/example

---
+++ examples/paths/src/index.ts
@@ -0,0 +1,8 @@
+/* 
+    Note: your editor/ide might be complaining about this import path as
+    it probably does not interpret .swcrc like it normally would a tsconfig.json,
+    you should safely be able to ignore this
+*/
+import { moduleA } from "@modules/moduleA";
+
+moduleA();
