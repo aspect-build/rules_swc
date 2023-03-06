0db4ce92c67d967ca82bed5d13a2c3f64013f24c
Jason Bedard <jason+github@jbedard.ca>
2023-03-06 07:29:08 -0600

    fix: set sourcemap root to the workspace relative root_dir (#176)

--- examples/filegroup/check_outputs.sh
+++ examples/filegroup/check_outputs.sh
@@ -5,10 +5,15 @@ cd "$TEST_SRCDIR/$TEST_WORKSPACE/$(dirname $TEST_TARGET)"
 
 grep "export var a" filegroup/a.js
 grep "sourceMappingURL=a.js.map" filegroup/a.js
-grep --fixed-strings '"sourceRoot":"examples/filegroup"' filegroup/a.js.map
+grep --fixed-strings '"sourceRoot":""' filegroup/a.js.map
 grep --fixed-strings '"sources":["a.ts"]' filegroup/a.js.map
 
 grep "export var b" filegroup/b.js
 grep "sourceMappingURL=b.js.map" filegroup/b.js
-grep --fixed-strings '"sourceRoot":"examples/filegroup"' filegroup/b.js.map
+grep --fixed-strings '"sourceRoot":""' filegroup/b.js.map
 grep --fixed-strings '"sources":["b.ts"]' filegroup/b.js.map
+
+grep "export var c" filegroup/sub/c.js
+grep "sourceMappingURL=c.js.map" filegroup/sub/c.js
+grep --fixed-strings '"sourceRoot":""' filegroup/sub/c.js.map
+grep --fixed-strings '"sources":["c.ts"]' filegroup/sub/c.js.map
