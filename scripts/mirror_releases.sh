6d721ccbb39dde325eb72583d115df7a7c072b0d
Greg Magolan <gmagolan@gmail.com>
2023-06-06 12:07:33 -0700

    chore: add version hashes for swc v1.3.44 through v1.3.61

--- scripts/mirror_releases.sh
+++ scripts/mirror_releases.sh
@@ -2,8 +2,16 @@
 
 set -o errexit -o nounset -o pipefail
 
-releases=$(curl -sSL -H 'Accept: application/vnd.github.v3+json' https://api.github.com/repos/swc-project/swc/releases?per_page=1 | jq -f $(pwd)/scripts/filter.jq)
-versions=$(echo $releases | jq --raw-output 'keys[]')
+if [ ${1:-} ]; then
+    # Fetch hashes of user supplied versions; these can be supplied as a list of arguments in the form:
+    # ./script/mirror_releases.sh v1.2.3 v1.2.4 v1.2.5
+    releases=$(curl -sSL -H 'Accept: application/vnd.github.v3+json' https://api.github.com/repos/swc-project/swc/releases?per_page=100 | jq -f $(pwd)/scripts/filter.jq)
+    versions=$@
+else
+    # Fetch hashes of just the latest version
+    releases=$(curl -sSL -H 'Accept: application/vnd.github.v3+json' https://api.github.com/repos/swc-project/swc/releases?per_page=1 | jq -f $(pwd)/scripts/filter.jq)
+    versions=$(echo $releases | jq --raw-output 'keys[]')
+fi
 
 for version in $versions; do
     echo "    \"$version\": {"
