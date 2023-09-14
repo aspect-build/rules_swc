cf9cd72a4ad1c3f1704a7a17f4fa4978a7207036
Vasilios Pantazopoulos <vpanta@vpanta.com>
2023-09-14 13:50:36 -0400

    Update mirror_releases.sh

--- scripts/mirror_releases.sh
+++ scripts/mirror_releases.sh
@@ -2,17 +2,29 @@
 
 set -o errexit -o nounset -o pipefail
 
-releases=$(curl -sSL -H 'Accept: application/vnd.github.v3+json' https://api.github.com/repos/swc-project/swc/releases?per_page=1 | jq -f $(pwd)/scripts/filter.jq)
+requested_version="${1-}"
+url="https://api.github.com/repos/swc-project/swc/releases"
+
+if [[ -z "$requested_version" ]]; then
+  url="${url}?per_page=1"
+else
+  # Get a lot of releases to match against.
+  url="${url}?per_page=50"
+fi
+
+releases=$(curl -sSL -H 'Accept: application/vnd.github.v3+json' $url | jq -f $(pwd)/scripts/filter.jq)
 versions=$(echo $releases | jq --raw-output 'keys[]')
 
 for version in $versions; do
-    echo "    \"$version\": {"
-    assets=$(echo $releases | jq --raw-output --arg v "$version" '.["\($v)"] | keys[]')
-    for asset in $assets; do
-        url=$(echo $releases | jq --raw-output --arg v "$version" --arg a "$asset" '.["\($v)"] | .["\($a)"]')
-        echo "        \"${asset%.exe}\": \"sha384-$(curl -sSL $url | shasum -b -a 384 | awk "{ print \$1 }" | xxd -r -p | base64)\","
-    done
-    echo "    },"
+    if [[ -z "$requested_version" ]] || [[ "$requested_version" == "$version" ]]; then
+        echo "    \"$version\": {"
+        assets=$(echo $releases | jq --raw-output --arg v "$version" '.["\($v)"] | keys[]')
+        for asset in $assets; do
+            url=$(echo $releases | jq --raw-output --arg v "$version" --arg a "$asset" '.["\($v)"] | .["\($a)"]')
+            echo "        \"${asset%.exe}\": \"sha384-$(curl -sSL $url | shasum -b -a 384 | awk "{ print \$1 }" | xxd -r -p | base64)\","
+        done
+        echo "    },"
+    fi
 done
 
 echo "Now, paste the above output into swc/private/versions.bzl as the first entry in TOOL_VERSIONS"
