ad0c92993e34a3156520ae91d3849ec51055745e
Alex Eagle <alex@aspect.dev>
2023-11-09 15:54:57 -0800

    feat: add a test that runs 'tsc' to produce the typescript source-of-truth.
    The SWC project claims that their emit should match, so this helps us reason about what is "correct" behavior.

---
+++ examples/paths/expected_tsc/modules/moduleB/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.moduleB = void 0;
+var moduleB = function () { return console.log("This is module B!"); };
+exports.moduleB = moduleB;
